254c6a5ae7b6a8657ee4a39c96398a7132e37061
lbs1123 <lbs7230@gmail.com>
2023-06-09 01:15:07 +0900

    state, props 연습

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
-  <div>
+  <!-- <div>
     <HeaderVue/>
     <div class="wrap">
       <MenuVue/>
       <ContentVue/>
     </div>
+  </div> -->
+
+  <div>
+    <BoxVue color="blue"/>
+    <BoxVue color="purple"/>
+    <BoxVue color="green"/>
+    <BoxVue color="blue"/>
+    <BoxVue color="purple"/>
+    <BoxVue/>
   </div>
 </template>
 
 <script>
-import HeaderVue from "./components/HeaderVue.vue"
-import MenuVue from "./components/MenuVue.vue"
-import ContentVue from "@/components/ContentVue.vue"
+// import HeaderVue from "./components/HeaderVue.vue"
+// import MenuVue from "./components/MenuVue.vue"
+// import ContentVue from "@/components/ContentVue.vue"
+import BoxVue from "@/components/BoxVue.vue"
 
 export default {
   name: 'App',
   components: {
-    HeaderVue,
-    MenuVue,
-    ContentVue,
+    // HeaderVue,
+    // MenuVue,
+    // ContentVue,
+    BoxVue,
   }
 }
 </script>
---
+++ src/components/BoxVue.vue
@@ -0,0 +1,35 @@
+<template>
+    <div
+        v-bind:class="['box', color]"
+        v-bind:style="{width: width + 'px', height: height + 'px'}" class="box"
+    ></div>
+</template>
+
+<script>
+export default {
+    props: {
+        color: { type: String, default: '' }
+    },
+    data() { //Box의 state
+        return {
+            width: 40,  //넓이
+            height: 80  //높이
+        };
+    }
+};
+</script>
+
+<style scoped>
+.box {
+  border: 1px solid #000;
+}
+.blue {
+  background: #009bff;
+}
+.purple {
+  background: #8f46ff;
+}
+.green {
+  background: #00bcac;
+}
+</style>
